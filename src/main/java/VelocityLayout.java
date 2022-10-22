81bf137776fae43b19802b28ff4798eae4fb1801
DiscountHomebrand <rmhall92@gmail.com>
2022-10-22 20:14:11 +1300

    VelocityLayout implementation

--- src/main/java/VelocityLayout.java
+++ src/main/java/VelocityLayout.java
@@ -1,6 +1,5 @@
 import org.apache.log4j.Layout;
 import org.apache.log4j.spi.LoggingEvent;
-import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
 
@@ -8,20 +7,36 @@ import java.io.StringWriter;
 import java.util.Date;
 
 public class VelocityLayout extends Layout {
-    protected Template template;
+    private static final String DEFAULT_PATTERN = "$m$n";
+    protected String pattern = DEFAULT_PATTERN;
+
+    public VelocityLayout() {
+    }
+
+    public VelocityLayout(String pattern) {
+        setPattern(pattern);
+    }
+
+    public void setPattern(String newPattern) {
+        if (newPattern == null) {
+            throw new IllegalArgumentException("Tried to set null pattern on VelocityLayout");
+        }
+
+        pattern = newPattern;
+    }
 
     @Override
     public String format(LoggingEvent loggingEvent) {
         VelocityContext context = new VelocityContext();
         context.put("c", loggingEvent.getLoggerName());
-        context.put("d", new Date(loggingEvent.getTimeStamp()));
+        context.put("d", new Date(loggingEvent.getTimeStamp()).toString());
         context.put("m", loggingEvent.getRenderedMessage());
-        context.put("p", loggingEvent.getLevel());
+        context.put("p", loggingEvent.getLevel().toString());
         context.put("t", loggingEvent.getThreadName());
         context.put("n", Layout.LINE_SEP);
 
         StringWriter stringWriter = new StringWriter();
-        template.merge(context, stringWriter);
+        Velocity.evaluate(context, stringWriter, "", pattern);
         return stringWriter.toString();
     }
 
